2d043672fe3dbb4578b13489bc287385a0c8217f
othmane.derrar <othmane.derrar@beyn.io>
2025-07-13 09:34:55 +0100

    only str validation

--- frontend/src/app/components/SearchInput.tsx
+++ frontend/src/app/components/SearchInput.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { useState } from 'react';
 
 interface Props {
@@ -9,10 +10,14 @@ interface Props {
 export default function SearchInput({ onSearch, loading }: Props) {
   const [query, setQuery] = useState('');
 
-  const handleClick = () => {
-    if (query.trim()) {
-      onSearch(query);
-    }
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const isValid = /^[a-zA-Z\s]*$/.test(value);
+    if (isValid) setQuery(value);
+  };
+
+  const handleSearchClick = () => {
+    if (query.trim()) onSearch(query.trim());
   };
 
   return (
@@ -21,11 +26,11 @@ export default function SearchInput({ onSearch, loading }: Props) {
         type="text"
         placeholder="Search a person"
         value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        onChange={handleInputChange}
         className="bg-slate-200 dark:bg-gray-100 p-3 rounded-xl w-full md:flex-1 text-gray-800 placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-[#3219EF]"
       />
       <button
-        onClick={handleClick}
+        onClick={handleSearchClick}
         disabled={loading || !query.trim()}
         className="bg-[#3219EF] text-white px-6 py-3 rounded-xl font-semibold hover:bg-indigo-700 transition disabled:opacity-50"
       >
